548432914d860c0576723e2d361e9fe0e57cdb92
ryhazh <rayidevs@gmail.com>
2025-05-08 09:33:58 +0700

    feat(UserController): add destroy method and fix code formatting

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,7 +18,7 @@ class UserController extends Controller
         try {
             $request->validate([
                 'name' => 'required|string|max:255',
-                'phone' =>'required|string|max:255',
+                'phone' => 'required|string|max:255',
                 'email' => 'required|email|unique:users',
                 'password' => 'required|string|min:6',
             ]);
@@ -37,25 +37,37 @@ class UserController extends Controller
         }
     }
 
-    public function update(Request $request, $id) {
+    public function update(Request $request, $id)
+    {
         try {
             $user = User::find($id);
 
             $request->validate([
-                'name' =>'required|string|max:255',
-                'phone' =>'required|string|max:255',
-                'email' =>'required|email|unique:users,email,'.$id,
-                'password' =>'nullable|string|min:6',
+                'name' => 'required|string|max:255',
+                'phone' => 'required|string|max:255',
+                'email' => 'required|email|unique:users,email,' . $id,
+                'password' => 'nullable|string|min:6',
             ]);
 
             $data = $request->except('password');
-            
+
             if ($request->filled('password')) {
                 $data['password'] = bcrypt($request->password);
             }
 
             $user->update($data);
             return redirect()->back()->with('success', 'User updated successfully');
+        } catch (\Throwable $th) {
+            return redirect()->back()->with('error', $th->getMessage());
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            $user = User::find($id);
+            $user->delete();
+            return redirect()->back()->with('success', 'User deleted successfully');
         } catch (\Throwable $th) {
             return redirect()->back()->with('error', $th->getMessage());
         }
