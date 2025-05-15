e177f9b9d192443dfa83adc19427535a9017e797
ryhazh <rayidevs@gmail.com>
2025-05-15 07:31:54 +0700

    feat: added new admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'role_id' => 1, // admin
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'phone' => '1234567890',
+            'password' => Hash::make('password123'),
+        ]);
+    }
+}
